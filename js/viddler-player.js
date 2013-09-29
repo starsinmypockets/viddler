f7a83c33d74a2e81746247df003d776cba88bd82
Phil <pives@ivesdigital.com>
2013-09-29 11:14:10 -0400

    docs changes

--- js/viddler-player.js
+++ js/viddler-player.js
@@ -1,17 +1,12 @@
 rainReady(function(){
-   	jwplayer("vid-jwplayer1").setup({
-        file: "../test/scroll_indicator.mp4",
-        height: 360,
-        image: "../test/scroll_indicator.png",
-        width: 640,
-        tracks: [
-	        { 
-	            file: "/test/thumbnails.vtt", 
-	            kind: "thumbnails"
-	        },
-	        { file: "../test/captions.vtt", kind: "subtitles", label: "English"}
-        ]
-    });
 
+    jwplayer('vid-jwplayer1').setup({
+        file: '/test/scroll_indicator.mp4',
+        plugins: {
+            '/js/sprites.js': {
+                text: 'Hello world!'
+            }
+        }
+    });
 
 });
