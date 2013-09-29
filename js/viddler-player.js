092465d65bc05f6bfc27b3f48279b6e9e0833e07
Phil <pives@ivesdigital.com>
2013-09-29 16:15:10 -0400

    adding backbone

--- js/viddler-player.js
+++ js/viddler-player.js
@@ -1,12 +1,5 @@
 rainReady(function(){
 
-    jwplayer('vid-jwplayer1').setup({
-        file: '/test/scroll_indicator.mp4',
-        plugins: {
-            '/js/sprites.js': {
-                text: 'Hello world!'
-            }
-        }
-    });
+    
 
 });
