c65e09c8237d5e69cc4a8ceaa698f7926a7ae073
Phil <pives@ivesdigital.com>
2013-10-07 16:13:29 -0400

    playerjs console

--- js/viddler-player.js
+++ js/viddler-player.js
@@ -1,3 +1,16 @@
+//ie console for dev
+ var alertFallback = true;
+   if (typeof console === "undefined" || typeof console.log === "undefined") {
+     console = {};
+     if (alertFallback) {
+         console.log = function(msg) {
+              alert(msg);
+         };
+     } else {
+         console.log = function() {};
+     }
+   }
+
 ( function ($) {
 var ViddlerPlayer = ViddlerPlayer || {};
 
