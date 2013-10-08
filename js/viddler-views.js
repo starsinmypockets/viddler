92d0da14eb6ed0afb80beeb1f6721648376abfe6
Phil <pives@ivesdigital.com>
2013-10-08 12:06:21 -0400

    view params

--- js/viddler-views.js
+++ js/viddler-views.js
@@ -95,7 +95,7 @@
                     // bind events once player is ready
                     that.onPlayerReady();
                 },
-                swfPath: "./js",
+                swfPath: "../skin/js",
                 supplied: "m4v, ogv",
                 errorAlerts : true
             });
