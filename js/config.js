4a5c37e9d0090238253759942ade2d1badbb20aa
Matthew Hurst <nyanmatt@gmail.com>
2013-10-23 12:30:11 -0400

    SiteCatalyst, LiveRail, and GA moved to plugin loader

---
+++ js/config.js
@@ -0,0 +1,29 @@
+var config = {};
+
+// Define plugins to be used here
+// foldername : Display Name
+config.plugins = {
+	
+	googleanalytics : function() {
+		// Google Analytics variables
+		config.ga_account = "";
+	}
+
+	/*
+	,sitecatalyst : function() {
+		// SiteCatalyst variables
+		config.s_account = "";
+		config.s_visitorNamespace = "";
+		config.s_trackingServer = "";
+	}
+	*/
+	
+	/*
+	,liverail : function() {
+		// LiveRail variables
+		// These will need to be populated by the api
+		config.lr_video_id = "asdf1234";
+		config.lr_title = "Big Buck Bunny";
+	}
+	*/
+};
---
+++ js/plugins/liverail/liverail-plugin.js
@@ -0,0 +1,41 @@
+// Required Config Values
+// config.lr_video_id
+// config.lr_title
+
+/*  <script language="javascript" src="http://cdn-static.liverail.com/js/LiveRail.Interstitial-1.0.js?LR_PUBLISHER_ID=1331&LR_VIDEO_ID=[contentID]&LR_TITLE=[contentTitle]&LR_LAYOUT_SKIN_ID=2&spacing=10"></script> */
+// This has to execute in the <head> since we're doing an inline document.write
+var fileref = document.createElement('script');
+fileref.src = "http://cdn-static.liverail.com/js/LiveRail.Interstitial-1.0.js?LR_PUBLISHER_ID=1331&LR_VIDEO_ID=" + config.lr_video_id + "&LR_TITLE=" + config.lr_title + "&LR_LAYOUT_SKIN_ID=2&spacing=10";
+document.documentElement.appendChild(fileref);
+
+//// LiveRail Events
+// initComplete, all ads are about to start
+function onLiveRailInitComplete() {
+  //alert('event="initComplete"');
+}
+
+// a campaign in the preroll playlist is about to begin
+function onLiveRailAdStart() {
+  //alert('event="adStart"');
+}
+
+// video ad has ended
+function onLiveRailAdEnd() {
+  //alert('event="adEnd"');
+}
+
+// user has clicked on the video ad
+function onLiveRailClickThru() {
+  //alert('event="clickThru"');
+}
+
+// all ads (if any available) have completed
+// proceed to main content
+function onLiveRailPrerollComplete(hasPlayedAd){
+  //alert('LiveRail complete. hasPlayedAd=' + hasPlayedAd);
+  if(hasPlayedAd){
+       // proceed to the main content
+  }else{
+       // daisy chain to some other ad-tag
+  }
+}
