6323f5997b7347edf7106734d9e39ccd6ca9ae7c
starsinmypockets <starsinmypockets@gmail.com>
2013-10-28 16:25:56 -0400

    Gray background works bettwe

--- js/viddler-views.js
+++ js/viddler-views.js
@@ -520,6 +520,7 @@ ie8 = function () {
                 console.log(seekPerc);
                 console.log(tlMs);
                 that.seekTo(tlMs);
+                return false;
             });
 
             for (var i = 0; i < tlSteps; i++) {
