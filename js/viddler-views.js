077955588f28750c97df22751e9d47f0cd326160
Phil <pives@ivesdigital.com>
2013-10-29 08:54:38 -0400

    fixed merge error that never loaded the Player

--- js/viddler-views.js
+++ js/viddler-views.js
@@ -347,6 +347,7 @@ ie8 = function () {
                 ViddlerPlayer.vent.once('playerReady', function () {
                     that.onPlayerReady();
                 });
+                that.vP.loadVPlayer();
             });
             
         },
