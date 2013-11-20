123980184e39572cbd0803d87a645d101e1a3628
starsinmypockets <starsinmypockets@gmail.com>
2013-11-20 10:20:35 -0500

    Organize manager methods

--- js/viddler-manager.js
+++ js/viddler-manager.js
@@ -25,38 +25,18 @@ define(['underscore', 'viddler-events'], function(_, Events) {
                 this.tlIndex = {};
             },
             
-            // reinitialize timeline
-            tlReset : function () {
-                this.tlStep = 0;
-                this.tlElapsed = 0;
-                this.tlNow = 0;
-            },
-            
-            setMediaEls : function (els) {
-                this.mediaEls = els;
-                this._initTlIndex();  // add some useful data to the manager object
-            },
-            
-            // @param t - time in ms relative to current step start time
-            setTime : function (t) {
-                data = {};
-                data.tlNow = this._getTlElapsed() + t;
-                data.tlNow = t;
-                this._updateTime(t);
-            },
-            
             
             /**
              * Plugins
              **/
             
             // @param opts - an array of plugin names
-            registerPlugins = function (opts) {
-                //
+            registerPlugins : function (plugins) {
                 var that = this;
-                _.each(opts, function (plugin) {
+                _.each(plugins, function (plugin) {
                     that.plugins[plugin] = { loaded : false }
-                })
+                });
+            },
                         
             // Update plugin status and check for allPluginsReady event condition
             // @param opts.plugin - the name of the plugin
@@ -77,63 +57,19 @@ define(['underscore', 'viddler-events'], function(_, Events) {
                     }
                 });
                 return allSet;
-            }
-            
-            _updateTime : function (t) {
-                Events.trigger('timeline:timeUpdate', t);
-                this.tlNow = t;
             },
             
-            getCurrentStep : function () {
-                return this.tlStep;
-            },
             
-            getStepStopTime : function () {
-                return this._getTlElapsed() + this.mediaEls[this.tlStep].elLength;
-            },
-            
-            incrementStep : function () {
-                    this.tlStep++;
-            },
-            
-            // return number of timeline steps
-            getTotalSteps : function () {
-                return this.mediaEls.length;
-            },
-            
-            // get total time of past timeline elements
-            _getTlElapsed : function () {
-                var tlElapsed = 0,
-                    that = this;
-                    
-                for (i = 0; i < that.tlStep; i++) {
-                    function func (i) {
-                        tlElapsed += that.mediaEls[i].elLength;
-                    }
-                    
-                    func(i);
-                }
-                return tlElapsed;
-            },
-            
-            getCurrentMedia : function () {
-                return this.mediaEls[this.tlStep];
-            },
-            
-            getMediaEls : function () {
-                return this.mediaEls;
-            },
-            
-            getTlIndex : function () {
-                return this.tlIndex;
-            },
-            
-            debug : function () {
-                console.log({
-                    'step' : this.tlStep,
-                    'stop' : this.getStepStopTime(),
-                    'now' : this.tlNow
-                });
+            /**
+             * Timing
+             **/
+             
+            // @param t - time in ms relative to current step start time
+            setTime : function (t) {
+                data = {};
+                data.tlNow = this._getTlElapsed() + t;
+                data.tlNow = t;
+                this._updateTime(t);
             },
             
             // convert timeline time to mediaElement and time
@@ -161,7 +97,32 @@ define(['underscore', 'viddler-events'], function(_, Events) {
                 console.log(el);
                 return this.mediaEls[el];
             },
+             
+            _updateTime : function (t) {
+                Events.trigger('timeline:timeUpdate', t);
+                this.tlNow = t;
+            },
             
+            // get total time of past timeline elements
+            _getTlElapsed : function () {
+                var tlElapsed = 0,
+                    that = this;
+                    
+                for (i = 0; i < that.tlStep; i++) {
+                    function func (i) {
+                        tlElapsed += that.mediaEls[i].elLength;
+                    }
+                    
+                    func(i);
+                }
+                return tlElapsed;
+            },
+            
+            
+            /**
+             * Timeline
+             **/
+             
             // index timeline elements for seek events
             _initTlIndex : function () {
                 var mediaEls = this.mediaEls,
@@ -182,7 +143,10 @@ define(['underscore', 'viddler-events'], function(_, Events) {
                 this.tlIndex = index;
             },
             
-            // @@ prob don't use playhead here
+            getTlIndex : function () {
+                return this.tlIndex;
+            },
+            
             // return tl length in ms
             getTlLength : function () {
                 var t = 0;
@@ -198,6 +162,58 @@ define(['underscore', 'viddler-events'], function(_, Events) {
                 seekPerc = clickX/($(e.currentTarget).width());
                 tlMs = seekPerc*this.getTlLength();
                 return tlMs;
+            },
+            
+            getCurrentStep : function () {
+                return this.tlStep;
+            },
+            
+            getStepStopTime : function () {
+                return this._getTlElapsed() + this.mediaEls[this.tlStep].elLength;
+            },
+            
+            incrementStep : function () {
+                    this.tlStep++;
+            },
+            
+            // return number of timeline steps
+            getTotalSteps : function () {
+                return this.mediaEls.length;
+            },
+            
+            // reinitialize timeline
+            tlReset : function () {
+                this.tlStep = 0;
+                this.tlElapsed = 0;
+                this.tlNow = 0;
+            },
+            
+            /**
+             * Media 
+             **/
+            
+            setMediaEls : function (els) {
+                this.mediaEls = els;
+                this._initTlIndex();  // add some useful data to the manager object
+            },
+            
+            getCurrentMedia : function () {
+                return this.mediaEls[this.tlStep];
+            },
+            
+            getMediaEls : function () {
+                return this.mediaEls;
+            },
+            
+            /**
+             * Utility
+             **/
+            debug : function () {
+                console.log({
+                    'step' : this.tlStep,
+                    'stop' : this.getStepStopTime(),
+                    'now' : this.tlNow
+                });
             },
         };
 
